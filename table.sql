97f8878dda11f34e470381da9b89d5e53df50989
ealpha072 <ealpha072@gmail.com>
2021-03-08 16:35:12 +0300

    Auto increment attribute added to id column

--- table.sql
+++ table.sql
@@ -1,2 +1,2 @@
 CREATE TABLE `files`.`users` 
-( `id` INT(100) NOT NULL , `name` VARCHAR(255) NOT NULL , `size` INT NOT NULL , `downloads` INT NOT NULL ) ENGINE = InnoDB;
+( `id` INT(100) NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL , `size` INT NOT NULL , `downloads` INT NOT NULL ) ENGINE = InnoDB;
