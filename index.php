bd9e5d4a3ed0140bd58542c69122898cdf6b8cf2
ealpha072 <ealpha072@gmail.com>
2021-03-08 15:46:33 +0300

    Initial design code

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>File system</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form">
+            <form action="" method="post">
+                <h3>Upload File</h3>
+                <div class="form-group">
+                    <label for="upload">Upload file</label>
+                    <input type="file" class="form-control-file">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary">Upload</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
