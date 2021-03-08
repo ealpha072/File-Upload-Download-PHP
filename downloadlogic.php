6d0451ba47a6d67bb60f6bf7169b39b24644e1bb
ealpha072 <ealpha072@gmail.com>
2021-03-08 17:34:04 +0300

    Full code for image download

--- downloadlogic.php
+++ downloadlogic.php
@@ -12,8 +12,43 @@
   
   $sql =$conn->prepare("SELECT * FROM users");
   $sql->execute();
-  
+
   $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+  if(isset($_GET['file_id'])){
+      $id =$_GET['file_id'];
+
+      //fetch file 
+    $sql =$conn->prepare("SELECT * FROM users WHERE id=$id");
+    $sql->execute();
+    $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+    $filepath ='uploads/'.$result[0]['name'];
+    
+    if(file_exists($filepath)){
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($filepath));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize('uploads/' . $result[0]['name']));
+        readfile('uploads/' . $file['name']);
+
+        //updating downloads
+        $newdownload = $result[0]['downloads'];
+        try {
+            $update =$conn->prepare("UPDATE users SET downloads=$newdownload WHERE id=$id");
+            $update->execute();
+            echo "Success, the download is now".$newdownload;
+            
+        } catch (\Throwable $e) {
+            //throw $th;
+            echo "Nop";
+            $e->getMessage();
+        }
+    }
+  }
 
 
 
