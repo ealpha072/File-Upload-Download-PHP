9a757a0ea906f3dc153ef3233936982c1867f96e
ealpha072 <ealpha072@gmail.com>
2023-08-23 11:30:00 +0300

    Clone of repo

--- process.php
+++ process.php
@@ -1,51 +1,50 @@
 <?php
-    $dsn = "mysql:host=localhost;dbname=files;charset=utf8mb4";
+	$dsn = "mysql:host=localhost;dbname=files;charset=utf8mb4";
     $username = "root";
     $password = "";
 
-   try{
-    $conn = new PDO($dsn,$username,$password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  }catch(Exception $e){
-    echo "Connection failed ".$e->getMessage();
-  }
+   	try{
+		$conn = new PDO($dsn,$username,$password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}catch(Exception $e){
+    	echo "Connection failed ".$e->getMessage();
+  	}
 
-  if (isset($_POST["upload"])) {
-      //we get the name
-      $filename =$_FILES["myfile"]["name"];
+  	if (isset($_POST["upload"])) {
+		//we get the name
+		$filename =$_FILES["myfile"]["name"];
 
-      //set file destination
-      $destination = "uploads/".$filename;
+		//set file destination
+		$destination = "uploads/".$filename;
 
-      //get file extension
-      $extension = pathinfo($filename, PATHINFO_EXTENSION);
+		//get file extension
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-      //ON THE UPLOADS directory
-      $file =$_FILES['myfile']['tmp_name'];
-      $size =$_FILES['myfile']['size'];
+		//ON THE UPLOADS directory
+		$file =$_FILES['myfile']['tmp_name'];
+		$size =$_FILES['myfile']['size'];
 
-      if(!in_array($extension, ['zip','pdf','png','jpg'])){
-        echo "Your file extension is not allowed";
-      }elseif ($_FILES['myfile']['size'] > 1000000){
-          echo "File too large";
+		if(!in_array($extension, ['zip','pdf','png','jpg'])){
+			echo "Your file extension is not allowed";
+		}elseif ($_FILES['myfile']['size'] > 1000000){
+			echo "File too large";
 
-      }else{
-          //move the uploaded file to its destination
-          if(move_uploaded_file($file,$destination)){
-            try {
-                //code...
-                $sql =$conn->prepare("INSERT INTO users(name, size, downloads) VALUES ( '$filename', '$size', '0')");
-                $sql->execute();
+		}else{
+			//move the uploaded file to its destination
+			if(move_uploaded_file($file,$destination)){
+				try {
+					//code...
+					$sql =$conn->prepare("INSERT INTO users(name, size, downloads) VALUES ( '$filename', '$size', '0')");
+					$sql->execute();
 
-                echo "Upload successfull!!!!!";
-              } catch (\Throwable $e) {
-                  //throw $th;
-                  echo "Failed ".$e->getMessage();
-              }
-          }else{
-              echo "Failed to upload file";
-          }
-      }
-  }
-
+					echo "Upload successfull!!!!!";
+				} catch (\Throwable $e) {
+					//throw $th;
+					echo "Failed ".$e->getMessage();
+				}
+			}else{
+				echo "Failed to upload file";
+			}
+		}
+  	}
 ?>
